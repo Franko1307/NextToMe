089ce17798ceb8933bf5de0877ea7b05a08d8ab9
franko1307 <franciscogonzalez1307@gmail.com>
2016-08-30 21:36:46 -0700

    nani

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var expressSession = require('express-session');
 var bodyParser  = require('body-parser')
 var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash')
+var configDB = require('./config/database.js');
 
-mongoose.connect('mongodb://localhost/test');
+require('./config/passport')(passport);
+
+mongoose.connect(configDB.url);
 
 
 var app = express();
@@ -11,10 +17,14 @@ app.use(express.static(__dirname + '/public'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(expressSession({secret: ''}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.set('view engine', 'jade')
 
-require('./routes/principal.js')(app)
+require('./routes/principal.js')(app,passport)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
--- routes/principal.js
+++ routes/principal.js
@@ -1,5 +1,16 @@
-module.exports = function (app) {
+module.exports = function (app, passport) {
   app.get('/', function(req, res) {
 		res.render('index', {name:"Fran"})
 	})
+  app.get('/sign-up', function(app, res){
+    res.render('sign_up')
+  })
+  app.post('/sign-up', passport.authenticate('local-sign-up', {
+    successRedirect: '/',
+    failureRedirect: '/sign-up',
+    failureFlash : true
+  }));
+  app.get('/*', function(app, res) {
+    res.render('index')
+  })
 }
